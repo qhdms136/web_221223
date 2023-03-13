e3f6061d052fe39932879143047c987c72f312ad
DESKTOP-PSQ9ERJ\장보은 <qhdms136@naver.com>
2023-03-13 17:23:47 +0900

    수정

--- src/main/java/com/test/lesson04/Quiz01.java
+++ src/main/java/com/test/lesson04/Quiz01.java
@@ -1,5 +1,10 @@
 package com.test.lesson04;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,11 +16,39 @@ import com.test.common.MysqlService;
 public class Quiz01 extends HttpServlet{
 	
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setContentType("text/plain");
 		
 		// db연결
 		MysqlService ms = MysqlService.getInstance();
 		ms.connect(); // ******
+		
+		// query insert
+		String insertQuery = "insert into `real_estate`(`realtorId`, `address`, `area`, `type`, `price`, `rentPrice`)"
+				+ "values(3, '헤라펠리스 101동 5305호', 350, '매매', 1500000, null)";
+		try {
+			ms.update(insertQuery);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		
+		// query select => 출력
+		PrintWriter out = response.getWriter();
+		String selectQuery = "select `address`, `area`, `type` from `real_estate`\r\n"
+				+ "order by `id` desc\r\n"
+				+ "limit 10";
+		try {
+			ResultSet res = ms.select(selectQuery);
+			while(res.next()) {
+				out.println("매물 주소: " + 
+			res.getString("address") + ", 면적 : " + res.getInt("area") + ", 타입: " + res.getString("type"));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// db 연결 해제
+		ms.disconnect();
 	}
 }
